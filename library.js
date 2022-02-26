e35bf918132f560d4e848ffc55e78268a2bfecba
Yosua Nicolaus <cryodawn257@gmail.com>
2022-02-26 16:16:22 +0700

    add Book constructor

---
+++ library.js
@@ -0,0 +1,12 @@
+const Library = [];
+
+function Book(title, author, pages, progress) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.progress = progress;
+}
+
+function addBookToLibrary() {
+
+}
