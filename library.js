613ee1a293cfc0f007758f8937aac04bcff5b81c
Yosua Nicolaus <cryodawn257@gmail.com>
2022-02-28 22:23:04 +0700

    clean and restructure code

--- library.js
+++ library.js
@@ -1,4 +1,6 @@
 const container = document.getElementById("container");
+const newButton = document.getElementById("new");
+const warning = document.getElementById("warning");
 const Library = [];
 
 function Book(title, author, pages, status) {
@@ -20,29 +22,25 @@ function addBookToLibrary(title, author, pages, status) {
   }
 }
 
-addBookToLibrary("Harry Potter", "JKR", "200", "Completed");
-addBookToLibrary("Hobbits", "Madeline", "300", "Reading");
-
-const newButton = document.getElementById("new");
 newButton.addEventListener("click", () => {
-  const title  = document.getElementById("title").value;
-  let   author = document.getElementById("author").value;
-  let   pages  = document.getElementById("pages").value;
+  const title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
   const status = document.getElementById("status").value;
 
   if (title.length == 0) {
-    newButton.nextElementSibling.style.display = "block";
+    warning.style.display = "block";
     return;
   }
-  newButton.nextElementSibling.style.display = "none";
+  warning.style.display = "none";
 
-  if (author.length == 0) {
-    author = "-";
-  }
-  if (pages.length == 0) {
-    pages = "-";
-  }
+  if (author.length == 0) author = "-";
+  if (pages.length == 0) pages = "-";
 
   addBookToLibrary(title, author, pages, status);
   document.querySelector("form").reset();
 });
+
+// sample book
+addBookToLibrary("Harry Potter", "JKR", "200", "Completed");
+addBookToLibrary("Hobbits", "Madeline", "300", "Reading");
