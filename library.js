ae7047b97f9a9c5e355e240dcf0892d7ebe2c7a0
Yosua Nicolaus <cryodawn257@gmail.com>
2022-03-02 10:58:36 +0700

    reorganize functions in code this is slowly turning into spagetthi code faster than I expected
    current problem:
    button's class keeps adding up
    so currentIndex is not accurate anymore

--- library.js
+++ library.js
@@ -13,6 +13,8 @@ const Library = [];
 let editIndex = -1;
 let editFocus = false;
 let currentIndex;
+let resetIndex = 0;
+let tr = document.querySelector("tr");
 
 function Book(title, author, pages, status) {
   this.title = title;
@@ -21,48 +23,94 @@ function Book(title, author, pages, status) {
   this.status = status;
 }
 
-function addBookToLibrary(title, author, pages, status) {
-  const newBook = new Book(title, author, pages, status);
-  Library.push(newBook);
-
-  const card = container.insertRow();
-  card.className = "card";
-  for (const key in newBook) {
-    const cell = card.insertCell();
-    cell.textContent = newBook[key];
+function displayLibrary() {
+  //reset display to 0
+  for (let i = 0; i < resetIndex; i++) {
+    tr = tr.nextElementSibling;
+    for (let i = 0; i < 5; i++) {
+      tr.deleteCell(0);
+    }
   }
+  resetIndex++;
 
-  let edit = card.insertCell();
-  let button = document.createElement("button");
-  button.textContent = "edit";
-  edit.appendChild(button);
+  //then fill the display with new Library entry
+  for (let i = 0; i < Library.length; i++) {
+    const card = container.insertRow();
+    card.className = "card";
+    for (const key in Library[i]) {
+      const cell = card.insertCell();
+      cell.textContent = Library[i][key];
+    }
+    card.insertCell();
+    addEdit(card);
+  }
+}
 
+function addEdit(rowCard) {
+  const card = rowCard;
+  const button = document.createElement("button");
   editIndex++;
+  button.textContent = "edit";
   button.className = "edit-" + editIndex;
+  card.lastChild.appendChild(button);
+
   button.addEventListener("click", () => {
+    console.log("helloooo");
     if (editFocus) {
       editFocus = false;
-      currentIndex = undefined;
-      card.style.filter = "brightness(100%)";
-      card.style.border = "none";
+      styleFocus(editFocus, currentIndex);
       form.reset();
       return;
     }
 
     editFocus = true;
-    //darken the library with that index
-    card.style.filter = "brightness(80%)";
-    card.style.border = "solid blue";
-
-    //fill the form and grab focus there
     currentIndex = button.className.slice(5);
+    styleFocus(editFocus, currentIndex);
+
     formTitle.value = Library[currentIndex].title;
     formAuthor.value = Library[currentIndex].author;
     formPages.value = Library[currentIndex].pages;
     formStatus.value = Library[currentIndex].status;
-
-    //rewrite the library with the data
   });
+}
+
+function editLibrary(button) {
+  if (editFocus) {
+    editFocus = false;
+    styleFocus(editFocus, currentIndex);
+    currentIndex = undefined;
+    form.reset();
+    return;
+  }
+
+  editFocus = true;
+  currentIndex = button.className.slice(5);
+  styleFocus(editFocus, currentIndex);
+
+  formTitle.value = Library[currentIndex].title;
+  formAuthor.value = Library[currentIndex].author;
+  formPages.value = Library[currentIndex].pages;
+  formStatus.value = Library[currentIndex].status;
+
+  //rewrite the library with the data
+}
+
+function addBookToLibrary(title, author, pages, status) {
+  const newBook = new Book(title, author, pages, status);
+  Library.push(newBook);
+
+  displayLibrary();
+}
+
+function styleFocus(isInFocus, index) {
+  const card = document.getElementsByClassName("card")[index];
+  if (isInFocus) {
+    card.style.filter = "brightness(80%)";
+    card.style.border = "solid blue";
+  } else {
+    card.style.filter = "brightness(100%)";
+    card.style.border = "none";
+  }
 }
 
 function submitBook(index) {
@@ -83,6 +131,8 @@ function submitBook(index) {
   if (editFocus) {
     editFocus = false;
     console.log("delete library with index " + currentIndex);
+    styleFocus(editFocus, currentIndex);
+    container.deleteRow(currentIndex);
   }
 
   addBookToLibrary(title, author, pages, status);
@@ -94,3 +144,6 @@ newButton.addEventListener("click", submitBook);
 // sample book
 addBookToLibrary("Harry Potter", "JKR", "200", "Completed");
 addBookToLibrary("Hobbits", "Madeline", "300", "Reading");
+addBookToLibrary("Adeleine", "John", "12", "Plan To Read");
+addBookToLibrary("What a Wonderful Day", "Plure", "110", "Reading");
+addBookToLibrary("You (not)", "Hera", "24", "On Hold");
